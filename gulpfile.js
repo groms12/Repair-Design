442f59be83d8a79dedb3908dcb738bf6b47e4c59
Marina <gms1204@mail.ru>
2020-03-02 19:32:30 +0400

    add min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const rename =  require('gulp-rename');
+const cssmin =  require('gulp-cssmin');
+ 
+gulp.task('default',  function ( ) {
+    return gulp.src('./*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+});
 
 gulp.task('hello', (done) => {
     console.log('hello, world!');
