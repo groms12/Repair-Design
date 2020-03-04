340acad345dfb70b430c21405fb9482e88853f42
Marina <gms1204@mail.ru>
2020-03-04 17:09:07 +0400

    feat: add gulp-sass in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,33 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const rename =  require('gulp-rename');
 const cssmin =  require('gulp-cssmin');
+const sass = require('gulp-sass');
  
-gulp.task('default',  function ( ) {
+function defalt() {
     return gulp.src('./*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
-});
+};
 
-gulp.task('hello', (done) => {
-    console.log('hello, world!');
-    done();
-});
-
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+    return src("./sass/*.scss")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
