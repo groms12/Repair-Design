085e08a8992ab2511e14b68bf0d19754ea00ca9d
Marina <gms1204@mail.ru>
2020-03-12 17:21:14 +0400

    feat: add jquery

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle="modal"]');
-    const closeBtn = document.querySelector('.modal__close');
-    const toggleModal = () => {
-        modal.classList.toggle('modal--visible');
-    };
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//     const modal = document.querySelector('.modal');
+//     const modalBtn = document.querySelectorAll('[data-toggle="modal"]');
+//     const closeBtn = document.querySelector('.modal__close');
+//     const toggleModal = () => {
+//         modal.classList.toggle('modal--visible');
+//     };
 
-    modalBtn.forEach(element => {
-        element.addEventListener('click', toggleModal);
+//     modalBtn.forEach(element => {
+//         element.addEventListener('click', toggleModal);
+//     });
+
+//     closeBtn.addEventListener('click', toggleModal)
+
+//     document.addEventListener('keydown', (e) => {
+//         if (e.keyCode === 27) {
+//             modal.classList.toggle('modal--visible');
+//         } 
+//     });
+// })
+
+$(document).ready(function () {
+    var modal = $('.modal');
+    var modalBtn = $('[data-toggle=modal]');
+    var closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
     });
-
-    closeBtn.addEventListener('click', toggleModal)
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
 
     document.addEventListener('keydown', (e) => {
         if (e.keyCode === 27) {
-            modal.classList.toggle('modal--visible');
+            modal.toggleClass('modal--visible');
         } 
     });
 });
