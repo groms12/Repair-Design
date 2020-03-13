d1677303ddfb5d85bf071964a510ed6b414dd80b
Marina <gms1204@mail.ru>
2020-03-13 23:13:22 +0400

    feat: add section projects and slider

--- js/main.js
+++ js/main.js
@@ -42,4 +42,23 @@ $(document).ready(function () {
             modal.toggleClass('modal--visible');
         }
     });
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    });
+    
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+
+    bullets.css('left', prev.width() + 15);
+    next.css('left', prev.width() + bullets.width() + 20);
 });
