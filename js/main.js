dfcd71c6db3b68c2155f28440d0c87b270a0f968
Marina <gms1204@mail.ru>
2020-03-22 16:36:26 +0400

    feat: add section our-team and our-clients

--- js/main.js
+++ js/main.js
@@ -225,36 +225,36 @@ $(document).ready(function () {
 
       $('[type=tel]').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
 
-      ymaps.ready(function () {
-        var myMap = new ymaps.Map('map', {
-                center: [47.244729, 39.723187],
-                zoom: 9
-            }, {
-                searchControlProvider: 'yandex#search'
-            }),
+    //   ymaps.ready(function () {
+    //     var myMap = new ymaps.Map('map', {
+    //             center: [47.244729, 39.723187],
+    //             zoom: 9
+    //         }, {
+    //             searchControlProvider: 'yandex#search'
+    //         }),
     
-            // Создаём макет содержимого.
-            MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-            ),
+    //         // Создаём макет содержимого.
+    //         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+    //             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    //         ),
     
-            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-                hintContent: 'Собственный значок метки',
-                balloonContent: 'Это красивая метка'
-            }, {
-                // Опции.
-                // Необходимо указать данный тип макета.
-                iconLayout: 'default#image',
-                // Своё изображение иконки метки.
-                iconImageHref: 'img/map.png',
-                // Размеры метки.
-                iconImageSize: [32, 32],
-                // Смещение левого верхнего угла иконки относительно
-                // её "ножки" (точки привязки).
-                iconImageOffset: [-5, -38]
-            });
+    //         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+    //             hintContent: 'Собственный значок метки',
+    //             balloonContent: 'Это красивая метка'
+    //         }, {
+    //             // Опции.
+    //             // Необходимо указать данный тип макета.
+    //             iconLayout: 'default#image',
+    //             // Своё изображение иконки метки.
+    //             iconImageHref: 'img/map.png',
+    //             // Размеры метки.
+    //             iconImageSize: [32, 32],
+    //             // Смещение левого верхнего угла иконки относительно
+    //             // её "ножки" (точки привязки).
+    //             iconImageOffset: [-5, -38]
+    //         });
     
-        myMap.geoObjects
-            .add(myPlacemark);
-    });
+    //     myMap.geoObjects
+    //         .add(myPlacemark);
+    // });
 });
